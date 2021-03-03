1a494b22f86661f65f588f56016e6b37847495a9
D. A <55723409+dariusakin20@users.noreply.github.com>
2021-03-02 21:02:45 -0500

    Timer
    Added timer can be put into any level.

---
+++ MathGameBenjamin/Assets/Scripts/UITimer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UITimer : MonoBehaviour
+{
+   public Text TimerText;
+   public bool playing;
+   private float Timer;
+
+    // Update is called once per frame
+    void Update()
+    {
+          if(playing == true){
+
+	  Timer += Time.deltaTime;
+	  int minutes = Mathf.FloorToInt(Timer / 60f);
+	  int seconds = Mathf.FloorToInt(Timer % 60f);
+	  int milliseconds = Mathf.FloorToInt((Timer * 100f) % 100f);
+      TimerText.text = ("Timer: ");
+	  TimerText.text = minutes.ToString ("00") + ":" + seconds.ToString ("00") + ":" + milliseconds.ToString("00");
+	}
+      
+    }
+}
