d29e2b833f06fb49ecb96b71908821143589bc78
Tuan Pham <tuan.programmer0101@gmail.com>
2021-03-20 08:39:49 -0400

    Fixed enemy movement

--- MathGameBenjamin/Assets/Scripts/EnemyScripts/Enemy.cs
+++ MathGameBenjamin/Assets/Scripts/EnemyScripts/Enemy.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] Transform groundCheckpos;
     [SerializeField] float moveSpeed = 1f;
+    [SerializeField] LayerMask groundLayer;
+
     Rigidbody2D myRigidBody;
     PolygonCollider2D damageCollider;
+    Collider2D bodyCollider;
     bool isAlive = true;
 
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
         damageCollider = GetComponent<PolygonCollider2D>();
+        bodyCollider = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
@@ -24,26 +29,29 @@ public class Enemy : MonoBehaviour
             return;
         }
 
-        if (IsFacingRight())
+        if (NeedToTurn())
         {
-            myRigidBody.velocity = new Vector2(moveSpeed, 0f);
+            Flip();
         }
-
-        else
-        {
-            myRigidBody.velocity = new Vector2(-moveSpeed, 0f);
-        }
-        
-        bool IsFacingRight()
-        {
-            return transform.localScale.x > 0;
-        }
+        Move();
         Die();
     }
 
-    private void OnTriggerExit2D(Collider2D collision)
+    void Move()
+    {
+        myRigidBody.velocity = new Vector2(moveSpeed, 0f);
+    }
+
+    void Flip()
     {
         transform.localScale = new Vector2(-(Mathf.Sign(myRigidBody.velocity.x)), 1f);
+        moveSpeed *= -1;
+    }
+
+    bool NeedToTurn()
+    {
+        return bodyCollider.IsTouchingLayers(groundLayer)  // hitting wall
+            || !Physics2D.OverlapCircle(groundCheckpos.position, 0.1f, groundLayer);  //facing edge
     }
 
     private void Die()
@@ -55,4 +63,6 @@ public class Enemy : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+
 }
