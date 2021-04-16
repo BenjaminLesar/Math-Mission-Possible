50fe2fd73379419a12756e32ff5a0c65b5ce0ce4
DESKTOP-5KO8I1L\David <sonofolorus@yahoo.com>
2021-04-15 21:31:46 -0500

    Level4Prelim

--- MathGameBenjamin/Assets/Scripts/GameManagerScripts/ExitMenu.cs
+++ MathGameBenjamin/Assets/Scripts/GameManagerScripts/ExitMenu.cs
@@ -7,7 +7,7 @@ public class ExitMenu : MonoBehaviour
     // Start is called before the first frame update
     public void Exit()
     {
-        //PlayerPrefs.SetString("IsNotFirst", "false");
+        PlayerPrefs.SetString("IsNotFirst", "false");
         GameObject[] PauseMenuCanvas = GameObject.FindGameObjectsWithTag("PauseMenuCanvas");
         GameObject myCanvas = PauseMenuCanvas[0].transform.GetChild(0).gameObject;
         myCanvas.SetActive(false);
