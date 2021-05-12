e66273501e6a0fd40b36ef23e9b4744c4b1cf610
SMLang03 <77689721+SMLang03@users.noreply.github.com>
2021-05-12 01:58:17 -0500

    World_001EndingScene
    -Created script: freezes player, plays laser, plays fader screen, enables finalPanel on entering EndTrigger.

---
+++ MathGameBenjamin/Assets/Scripts/UIScripts/World_001EndScene.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World_001EndScene : MonoBehaviour
+{
+    
+    public GameObject player;
+    private ParticleSystem laser;
+    public ParticleSystem teleporter;
+    public GameObject finalPanel;
+    public Animator faderScreen;
+
+    IEnumerator timer()
+    {
+        Player.instance.FreezePlayer();
+        laser = gameObject.GetComponent<ParticleSystem>();
+        laser.Play();
+        faderScreen.SetBool("fade", true);
+        yield return new WaitForSeconds(4);
+        finalPanel.SetActive(true);
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        StartCoroutine(timer());
+    }
+}
