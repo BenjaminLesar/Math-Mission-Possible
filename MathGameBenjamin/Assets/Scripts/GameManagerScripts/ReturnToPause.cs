623125035a99737ea5e944b41fc577338ced77ce
DESKTOP-5KO8I1L\David <sonofolorus@yahoo.com>
2021-05-16 19:35:04 -0500

    ReturnToMainButton

---
+++ MathGameBenjamin/Assets/Scripts/GameManagerScripts/ReturnToPause.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnToPause : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Back()
+    {
+        GameObject ReturnCanvas = gameObject.transform.parent.gameObject.transform.parent.gameObject;
+        GameObject PauseMenuCanvas = ReturnCanvas.transform.parent.gameObject.transform.GetChild(0).gameObject;
+        PauseMenuCanvas.SetActive(true);
+        ReturnCanvas.SetActive(false);
+    }
+}
---
+++ MathGameBenjamin/Assets/Scripts/GameManagerScripts/ReturnYesOrNo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnYesOrNo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void AskReturn()
+    {
+        GameObject PauseCanvasParent = gameObject.transform.parent.gameObject.transform.parent.gameObject.transform.parent.gameObject;
+        GameObject PauseMenuCanvas = PauseCanvasParent.transform.GetChild(0).gameObject;
+        GameObject ReturnCanvas = PauseCanvasParent.transform.GetChild(1).gameObject;
+        ReturnCanvas.SetActive(true);
+        PauseMenuCanvas.SetActive(false);
+    }
+}
