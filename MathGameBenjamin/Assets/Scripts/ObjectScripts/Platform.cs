271b331f9bd59ca1528ecfc7897eca96dd54522b
DESKTOP-5KO8I1L\David <sonofolorus@yahoo.com>
2021-05-20 20:19:44 -0500

    Flip Fish

--- MathGameBenjamin/Assets/Scripts/ObjectScripts/Platform.cs
+++ MathGameBenjamin/Assets/Scripts/ObjectScripts/Platform.cs
@@ -6,15 +6,12 @@ public class Platform : MonoBehaviour
 {
     public Transform pos1, pos2;
     public float speed;
-
     Vector3 nextPos;
 
     // Start is called before the first frame update
     void Start()
     {
-
         nextPos = pos1.position;
-        
     }
 
     // Update is called once per frame
@@ -23,12 +20,20 @@ public class Platform : MonoBehaviour
         if(transform.position == pos1.position){
 
             nextPos = pos2.position;
+            if(gameObject.layer == 12)
+            {
+                gameObject.GetComponent<SpriteRenderer>().flipX = false;
+            }
 
         }     
 
         if(transform.position == pos2.position){
 
             nextPos = pos1.position;
+            if (gameObject.layer == 12)
+            {
+                gameObject.GetComponent<SpriteRenderer>().flipX = true;
+            }
         }  
 
         transform.position = Vector3.MoveTowards(transform.position, nextPos, speed * Time.deltaTime);
